d99fb44cc189185510108b0d722ea9f3e937a7fe
Bala Paranj <bparanj@gmail.com>
2017-03-23 19:29:17 -0700

    Login and Logout with user name

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
+  resources :sessions
+  
   root 'welcome#index'
 end
